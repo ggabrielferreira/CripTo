12e09c4bb2ee0aa7ef56aaad977deddb9200f283
ga3riel-ferreira <ferreiragabrielgoncalves@gmail.com>
2022-05-05 20:50:12 -0300

    Alterando estilo do botão copiar ao clicar

--- js/copia.js
+++ js/copia.js
@@ -4,15 +4,8 @@ botaoCopia.addEventListener("click", function(){
     copiar(saida);
 });
 
-entrada.addEventListener("paste", function(){
-    botaoCopia.classList.remove("copiado");
-    botaoCopia.textContent = "Copiar"
-});
-
 function copiar(saida) {
     let textoCopiado = saida;
     textoCopiado.focus();
     navigator.clipboard.writeText(textoCopiado.textContent);
-    botaoCopia.classList.add("copiado");
-    botaoCopia.textContent = "Copiado!"
 }

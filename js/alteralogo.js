d80fa8fc900b71890000d16b5e34116f74bab2cd
ga3riel-ferreira <ferreiragabrielgoncalves@gmail.com>
2022-05-11 15:37:51 -0300

    Adicionando arquivos de imagem para o readme.md

--- js/alteralogo.js
+++ js/alteralogo.js
@@ -16,7 +16,9 @@ window.addEventListener("resize",function(){
     larguraDoDispositivo = window.screen.width;
     if(larguraDoDispositivo >= 900) {
         logo.setAttribute('src', 'images/cripTo.png');
+        logo.setAttribute('alt', 'logo do site: CripTo com um gradiente verde. Logo embaixo, vários 0 e 1 com o mesmo gradiente');
     } else {
         logo.setAttribute('src', 'images/cripToIcon.png');
+        logo.setAttribute('alt', 'logo do site: um grande c preenchindo com um gradiente verde, cortado na diagonal');
     }
 })

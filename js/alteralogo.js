f5c58611cf9f79aeff64cf93c5415732e97a86a8
ga3riel-ferreira <ferreiragabrielgoncalves@gmail.com>
2022-05-11 13:54:51 -0300

    Valores do atríbuto alt da logo do site agora são ajustados de acordo com o dispositivo

--- js/alteralogo.js
+++ js/alteralogo.js
@@ -4,16 +4,18 @@ let logo = selecionarElemento("#logo");
 window.addEventListener("load",function(){
     larguraDoDispositivo = window.screen.width;
     if(larguraDoDispositivo >= 900) {
-        logo.setAttribute('src', 'images/cripthis.png');
+        logo.setAttribute('src', 'images/cripTo.png');
+        logo.setAttribute('alt', 'logo do site: CripTo com um gradiente verde. Logo embaixo, vários 0 e 1 com o mesmo gradiente');
     } else {
         logo.setAttribute('src', 'images/cripToIcon.png');
+        logo.setAttribute('alt', 'logo do site: um grande c preenchindo com um gradiente verde, cortado na diagonal');
     }
 })
 
 window.addEventListener("resize",function(){
     larguraDoDispositivo = window.screen.width;
     if(larguraDoDispositivo >= 900) {
-        logo.setAttribute('src', 'images/cripthis.png');
+        logo.setAttribute('src', 'images/cripTo.png');
     } else {
         logo.setAttribute('src', 'images/cripToIcon.png');
     }
